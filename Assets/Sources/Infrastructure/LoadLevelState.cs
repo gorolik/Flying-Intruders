12aca1890920daaebdeb686e0febd7bca7e4ec65
gorolik <govorkov05@mail.ru>
2023-08-07 16:07:56 +0700

    Вынесение констант путей к префабам

--- Assets/Sources/Infrastructure/LoadLevelState.cs
+++ Assets/Sources/Infrastructure/LoadLevelState.cs
@@ -5,6 +5,8 @@ namespace Sources.Infrastructure
     internal class LoadLevelState : IPayloadState<string>
     {
         private const string _spawnPointTag = "SpawnPoint";
+        private const string _weaponPath = "Weapon";
+        private const string _hudPath = "HUD";
         private readonly GameStateMachine _gameStateMachine;
         private readonly SceneLoader _sceneLoader;
 
@@ -28,8 +30,8 @@ namespace Sources.Infrastructure
         {
             GameObject spawnPoint = GameObject.FindGameObjectWithTag(_spawnPointTag);
 
-            var weapon = Instantiate("Weapon", spawnPoint.transform.position);
-            var hud = Instantiate("HUD");
+            var weapon = Instantiate(_weaponPath, spawnPoint.transform.position);
+            var hud = Instantiate(_hudPath);
         }
 
         private GameObject Instantiate(string path, Vector3 position)
